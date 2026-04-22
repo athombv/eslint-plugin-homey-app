61475b4197694cf977dc5cca48fe30ed738b28fe
Wouter Kamp <wouter.kamp@athom.nl>
2026-04-22 09:53:03 +0200

    dropped legacy support in favor of major version bump

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const globalTimersRule = require('./rules/global-timers');
 const plugin = {
   meta: {
     name: 'eslint-plugin-homey-app',
-    version: '1.1.0',
+    version: '2.0.0',
   },
   configs: {},
   rules: {
@@ -14,17 +14,10 @@ const plugin = {
 };
 
 Object.assign(plugin.configs, {
-  'flat/recommended': {
+  recommended: {
     plugins: {
       'homey-app': plugin,
     },
-    rules: {
-      'homey-app/global-timers': 'warn',
-    },
-  },
-
-  recommended: {
-    plugins: ['homey-app'],
     rules: {
       'homey-app/global-timers': 'warn',
     },
